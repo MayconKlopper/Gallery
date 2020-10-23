08a099247a5c0fe106360a2d343350c6bed6058c
Maycon Klopper de Carvalho <mayconcarvalho@brq.com>
2020-10-23 12:52:46 -0300

    Implementação ícone android

--- Gallery/Gallery.Android/MainActivity.cs
+++ Gallery/Gallery.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Gallery.Droid
 {
-    [Activity(Label = "Gallery", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Gallery", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
